6f803d4f350ada544aae4ec55de5ce3d0d1a8039
unknown <crimm_32@hotmail.com>
2022-05-24 17:14:14 -0400

    first commit

---
+++ src/BubbleSort.java
@@ -0,0 +1,13 @@
+class BubbleSort {
+    public static int[] bubbleSort(int arr[]) {
+        int size = arr.length;
+        for (int i = 0; i < size - 1; i++)
+            for (int j = 0; j < size - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+        return arr;
+    }
+}
